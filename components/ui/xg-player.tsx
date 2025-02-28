0880694526da56a6356bc58ad89b982be40b5b53
wood chen <wood@czl.net>
2025-03-01 05:30:01 +0800

    取消对于苹果设备的检测, 统一使用HlsPlugin

--- components/ui/xg-player.tsx
+++ components/ui/xg-player.tsx
@@ -36,50 +36,28 @@ function XGPlayerInner({ url }: XGPlayerProps) {
       ? videoProgress.currentTime
       : 0
     const player = url.includes('.m3u8')
-      ? (typeof document !== 'undefined' && document.createElement('video').canPlayType('application/vnd.apple.mpegurl'))
-        ? new Player({
-            id: 'xgplayer',
-            url,
-            autoplay: true,
-            volume: 1,
-            playbackRate: [0.5, 0.75, 1, 1.5, 2],
-            pip: true,
-            airplay: true,
-            inactive: 5000,
-            startTime,
-            errorTips: `请<span>切换其他播放线路</span>试试`,
-            enableVideoDbltouch: true,
-            playsinline: true,
-            leavePlayerTime: 5,
-            cssFullscreen: false,
-            fullscreen: true,
-            rotate: true,
-            videoAttributes: {
-              crossOrigin: 'anonymous'
-            }
-          })
-        : new Player({
-            id: 'xgplayer',
-            url,
-            autoplay: true,
-            volume: 1,
-            playbackRate: [0.5, 0.75, 1, 1.5, 2],
-            pip: true,
-            airplay: true,
-            inactive: 5000,
-            startTime,
-            errorTips: `请<span>切换其他播放线路</span>试试`,
-            enableVideoDbltouch: true,
-            playsinline: true,
-            leavePlayerTime: 5,
-            cssFullscreen: false,
-            fullscreen: true,
-            rotate: true,
-            videoAttributes: {
-              crossOrigin: 'anonymous'
-            },
-            plugins: [HlsPlugin]
-          })
+      ? new Player({
+          id: 'xgplayer',
+          url,
+          autoplay: true,
+          volume: 1,
+          playbackRate: [0.5, 0.75, 1, 1.5, 2],
+          pip: true,
+          airplay: true,
+          inactive: 5000,
+          startTime,
+          errorTips: `请<span>切换其他播放线路</span>试试`,
+          enableVideoDbltouch: true,
+          playsinline: true,
+          leavePlayerTime: 5,
+          cssFullscreen: false,
+          fullscreen: true,
+          rotate: true,
+          videoAttributes: {
+            crossOrigin: 'anonymous'
+          },
+          plugins: [HlsPlugin]
+        })
       : url.includes('.flv')
         ? new Player({
             id: 'xgplayer',
